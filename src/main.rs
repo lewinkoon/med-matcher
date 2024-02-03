2013ad2f8641e0e056e18d4eddabbe322fe638c7
Lewin <57636993+lewinkoon@users.noreply.github.com>
2024-02-03 22:20:39 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use polars::prelude::*;
+use std::fs;
+
+fn main() {
+    let df: DataFrame = CsvReader::from_path("files/applicants.csv")
+        .unwrap()
+        .finish()
+        .unwrap();
+
+    // check if the output folder exists
+    let output_path = "output";
+    if !folder_exists(&output_path) {
+        // if it does not exist then create it
+        if let Err(err) = create_folder(&output_path) {
+            eprintln!("Error creating output folder: {}", err);
+        } else {
+            println!("Output folder created succesfully!");
+        }
+    }
+
+    // get priority columns as a list
+    let priorities: Vec<&str> = df
+        .get_column_names()
+        .iter()
+        .filter(|&x| x.contains("ORDEN"))
+        .cloned()
+        .collect();
+
+    // disaggregate and merges tables
+    let board = df
+        .clone()
+        .melt(["SOLICITANTE"], priorities)
+        .unwrap()
+        .drop("value")
+        .unwrap()
+        .rename("variable", "PRIORIDAD")
+        .unwrap()
+        .join(
+            &df.clone(),
+            ["SOLICITANTE"],
+            ["SOLICITANTE"],
+            JoinArgs::new(JoinType::Inner),
+        )
+        .unwrap();
+
+    println!("{}", &board);
+}
+
+fn folder_exists(path: &str) -> bool {
+    fs::metadata(path)
+        .map(|metadata| metadata.is_dir())
+        .unwrap_or(false)
+}
+
+fn create_folder(path: &str) -> Result<(), std::io::Error> {
+    fs::create_dir(path)
+}
