1d5b15a424628e1467668c02826cde813dc70fd7
Lewin <57636993+lewinkoon@users.noreply.github.com>
2024-03-25 19:21:46 +0100

    read `applicants.csv` file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 mod helpers;
-
 extern crate csv;
 
-// use std::error::Error;
+use csv::Reader;
 use std::fs::File;
 
-fn main() {
-    // open csv file
-    let _file = File::open("files/applicants.csv");
+fn main() -> Result<(), csv::Error> {
+    let mut rdr: Reader<File> = csv::Reader::from_path("./files/applicants.csv")?;
+
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
 }
