e69354acbc32e17ede7eac379577ae9c20fc74d9
Lewin <57636993+lewinkoon@users.noreply.github.com>
2024-03-27 22:20:15 +0100

    flatten input data

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use ::serde::Deserialize;
+use serde::de::DeserializeOwned;
 use std::error::Error;
 
 #[allow(dead_code)]
 #[derive(Deserialize)]
 pub struct Request {
     applicant: String,
+    vacancy: String,
+    preference: u8,
     name: String,
     surname1: String,
     surname2: String,
@@ -15,56 +18,33 @@ pub struct Request {
     forced_movility: bool,
     excluded: bool,
     reason: Option<u8>,
-    preference01: String,
-    preference02: String,
-    preference03: String,
     total_worked_time: u8,
     permanent_worked_time: u8,
     emergencies_experience: u8,
     emergencies_training: u8,
 }
 
-impl Request {
-    pub fn build(file_path: &str) -> Result<Vec<Request>, Box<dyn Error>> {
-        // read csv file
-        let mut rdr = csv::Reader::from_path(file_path)?;
-
-        // push each csv row into a vector of structs
-        let mut records: Vec<Request> = Vec::new();
-        for result in rdr.deserialize() {
-            let record: Request = result?;
-            records.push(record);
-        }
-        Ok(records)
-    }
+#[allow(dead_code)]
+#[derive(Deserialize)]
+pub struct Vacancy {
+    vacancy: String,
+    quantity: u8,
+    shift: String,
+    department: String,
+    characteristics: String,
+    specialty: String,
+    profile: String,
 }
 
-// #[derive(Debug, Deserialize)]
-// struct Vacancy {
-//     vacancy: String,
-//     quantity: u8,
-//     shift: String,
-//     department: String,
-//     characteristics: String,
-//     specialty: String,
-//     profile: String,
-// }
+pub fn parse_file<T: DeserializeOwned>(file_path: &str) -> Result<Vec<T>, Box<dyn Error>> {
+    // read csv file
+    let mut rdr = csv::Reader::from_path(file_path)?;
 
-// struct Board {
-//     applicant: String,
-//     vacancy: String,
-//     name: String,
-//     surname1: String,
-//     surname2: String,
-//     birthday: String,
-//     admission_date: String,
-//     specialty: Option<String>,
-//     preference_request: bool,
-//     forced_movility: bool,
-//     excluded: bool,
-//     reason: Option<u8>,
-//     total_worked_time: u8,
-//     permanent_worked_time: u8,
-//     emergencies_experience: u8,
-//     emergencies_training: u8,
-// }
+    // push each csv row into a vector of structs
+    let mut records: Vec<T> = Vec::new();
+    for result in rdr.deserialize() {
+        let record = result?;
+        records.push(record);
+    }
+    Ok(records)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use log::info;
-use medmatch::Request;
+use medmatch::*;
 use std::process;
 
 fn main() {
@@ -8,37 +8,23 @@ fn main() {
 
     // read applicants requests
     let applicants_path: &str = "files/applicants.csv";
-    let applicants = Request::build(applicants_path).unwrap_or_else(|err| {
+    let applicants: Vec<Request> = parse_file(applicants_path).unwrap_or_else(|err| {
         println!("{err}");
         process::exit(1);
     });
     info!("Loaded {} applicant requests.", applicants.len());
 
     // read vacant positions
-    // let vacancies_path: &str = "files/applicants.csv";
-    // let vacancies: Vec<Vacancy> = read_file(vacancies_path).unwrap();
-    // info!("Vacancies data was read successfully.");
+    let vacancies_path: &str = "files/vacancies.csv";
+    let vacancies: Vec<Vacancy> = parse_file(vacancies_path).unwrap_or_else(|err| {
+        println!("{err}");
+        process::exit(1);
+    });
+    info!("Loaded {} vacancy positions.", vacancies.len());
 
-    // let mut test: Vec<Board> = Vec::new();
+    // let mut test: Vec<Request> = Vec::new();
     // for item in applicants.into_iter() {
-    //     let row = Board {
-    //         applicant: item.applicant,
-    //         vacancy: String::new(),
-    //         name: String::new(),
-    //         surname1: String::new(),
-    //         surname2: String::new(),
-    //         birthday: String::new(),
-    //         admission_date: String::new(),
-    //         specialty: Some(String::new()),
-    //         preference_request: false,
-    //         forced_movility: false,
-    //         excluded: false,
-    //         reason: Some(0),
-    //         total_worked_time: 0,
-    //         permanent_worked_time: 0,
-    //         emergencies_experience: 0,
-    //         emergencies_training: 0,
-    //     };
+    //     let row = Request::build(item.applicant);
     //     test.push(row);
     // }
 }
