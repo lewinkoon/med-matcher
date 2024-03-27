e781767b10c6c6f54e22f4f659377b17ff1ee1f2
Lewin <lewinkoon+github@gmail.com>
2024-03-27 18:35:16 +0100

    define `Board` object

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use log::{error, info};
-use serde::Deserialize;
+use log::info;
+use serde::{de::DeserializeOwned, Deserialize};
 use std::error::Error;
 
-#[derive(Debug, Deserialize, PartialEq, PartialOrd, Eq, Ord)]
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
 struct Request {
     applicant: String,
     name: String,
@@ -24,19 +25,49 @@ struct Request {
     emergencies_training: u8,
 }
 
-fn read_file(file_path: &str) -> Result<Vec<Request>, Box<dyn Error>> {
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Vacancy {
+    vacancy: String,
+    quantity: u8,
+    shift: String,
+    department: String,
+    characteristics: String,
+    specialty: String,
+    profile: String,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Board {
+    applicant: String,
+    vacancy: String,
+    name: String,
+    surname1: String,
+    surname2: String,
+    birthday: String,
+    admission_date: String,
+    specialty: Option<String>,
+    preference_request: bool,
+    forced_movility: bool,
+    excluded: bool,
+    reason: Option<u8>,
+    total_worked_time: u8,
+    permanent_worked_time: u8,
+    emergencies_experience: u8,
+    emergencies_training: u8,
+}
+
+fn read_file<T: DeserializeOwned>(file_path: &str) -> Result<Vec<T>, Box<dyn Error>> {
     // read csv file
     let mut rdr = csv::Reader::from_path(file_path)?;
 
     // push each csv row into a vector of structs
-    let mut records: Vec<Request> = Vec::new();
+    let mut records: Vec<T> = Vec::new();
     for result in rdr.deserialize() {
-        let record: Request = result?;
+        let record: T = result?;
         records.push(record);
     }
-
-    // sort records by name
-    records.sort_by(|a, b| b.name.cmp(&a.name));
 
     Ok(records)
 }
@@ -45,10 +76,34 @@ fn main() {
     // initialize logger
     env_logger::init();
 
-    let path = "files/applicants.csv";
+    // read applicants requests
+    let applicants_path: &str = "files/applicants.csv";
+    let _vacancies_path: &str = "files/applicants.csv";
+    let applicants: Vec<Request> = read_file(applicants_path).unwrap();
+    info!("Applicants data was read successfully.");
+    // let _vacancies: Vec<Vacancy> = read_file(vacancies_path).unwrap();
+    // info!("Vacancies data was read successfully.");
 
-    match read_file(path) {
-        Ok(res) => info!("Script finished succesfully: {:?}", res[0]),
-        Err(error) => error!("Script crashed: {}", error),
-    };
+    let mut test: Vec<Board> = Vec::new();
+    for item in applicants.into_iter() {
+        let row = Board {
+            applicant: item.applicant,
+            vacancy: String::new(),
+            name: String::new(),
+            surname1: String::new(),
+            surname2: String::new(),
+            birthday: String::new(),
+            admission_date: String::new(),
+            specialty: Some(String::new()),
+            preference_request: false,
+            forced_movility: false,
+            excluded: false,
+            reason: Some(0),
+            total_worked_time: 0,
+            permanent_worked_time: 0,
+            emergencies_experience: 0,
+            emergencies_training: 0,
+        };
+        test.push(row);
+    }
 }
