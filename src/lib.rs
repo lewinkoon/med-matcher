4133832c9fded0373a1cd6b75786a7826800f9e2
Lewin <57636993+lewinkoon@users.noreply.github.com>
2024-03-28 18:06:27 +0100

    reimplemen serde

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,35 @@
-use csv::StringRecord;
+use serde::de::DeserializeOwned;
+use serde::{Deserialize, Serialize};
 use std::error::Error;
 
-#[allow(dead_code)]
+pub fn parse_file<T: DeserializeOwned>(file_path: &str) -> Result<Vec<T>, Box<dyn Error>> {
+    // read csv file
+    let mut rdr = csv::Reader::from_path(file_path)?;
+
+    // push each csv row into a vector of structs
+    let mut records = Vec::new();
+    for result in rdr.deserialize() {
+        let record = result?;
+        records.push(record);
+    }
+    Ok(records)
+}
+
+pub fn export(file_path: &str, data: Vec<Request>) -> Result<(), Box<dyn Error>> {
+    let mut wtr = csv::WriterBuilder::new()
+        .has_headers(false)
+        .from_path(file_path)?;
+    wtr.write_record(headers())?;
+    for record in data {
+        wtr.serialize(record)?;
+    }
+    wtr.flush()?;
+    Ok(())
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Applicant {
-    applicant: String,
+    id: String,
     name: String,
     surname1: String,
     surname2: String,
@@ -11,55 +37,25 @@ pub struct Applicant {
     specialty: String,
 }
 
-#[allow(dead_code)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Vacancy {
-    vacancy: String,
-    quantity: u8,
+    id: String,
+    quantity: String,
     shift: String,
     department: String,
     specialty: String,
     profile: String,
 }
 
-#[allow(dead_code)]
+#[derive(Debug, Serialize)]
 pub struct Request<'a> {
     applicant: &'a Applicant,
     vacancy: &'a Vacancy,
     preference: String,
 }
 
-pub trait Record {
-    fn new(row: StringRecord) -> Self;
-}
-
-impl Record for Applicant {
-    fn new(row: StringRecord) -> Self {
-        Self {
-            applicant: row.get(0).unwrap().to_string(),
-            name: row.get(1).unwrap().to_string(),
-            surname1: row.get(2).unwrap().to_string(),
-            surname2: row.get(3).unwrap().to_string(),
-            birthday: row.get(4).unwrap().to_string(),
-            specialty: row.get(6).unwrap().to_string(),
-        }
-    }
-}
-
-impl Record for Vacancy {
-    fn new(row: StringRecord) -> Self {
-        Self {
-            vacancy: row.get(0).unwrap().to_string(),
-            quantity: row.get(1).unwrap().parse().unwrap(),
-            shift: row.get(2).unwrap().to_string(),
-            department: row.get(3).unwrap().to_string(),
-            specialty: row.get(4).unwrap().to_string(),
-            profile: row.get(5).unwrap().to_string(),
-        }
-    }
-}
-
 impl<'a> Request<'a> {
-    pub fn new(row: StringRecord, app: &'a [Applicant], vac: &'a [Vacancy]) -> Self {
+    pub fn build(row: csv::StringRecord, app: &'a [Applicant], vac: &'a [Vacancy]) -> Self {
         let app_idx = row.get(0).unwrap()[1..]
             .parse::<usize>()
             .unwrap_or_default();
@@ -76,33 +72,22 @@ impl<'a> Request<'a> {
             preference: row.get(2).unwrap().to_string(),
         }
     }
-    fn format(&self) -> [&str; 4] {
-        let applicant = &self.applicant.applicant;
-        let name = &self.applicant.name;
-        let vacancy = &self.vacancy.vacancy;
-        let preference = &self.preference;
-        [applicant, vacancy, name, preference]
-    }
 }
 
-pub fn parse_file<T: Record>(file_path: &str) -> Result<Vec<T>, Box<dyn Error>> {
-    // read csv file
-    let mut rdr = csv::Reader::from_path(file_path)?;
-
-    // push each csv row into a vector of structs
-    let mut records = Vec::new();
-    for result in rdr.records() {
-        let record = Record::new(result?);
-        records.push(record);
-    }
-    Ok(records)
-}
-
-pub fn export(file_path: &str, data: Vec<Request>) -> Result<(), Box<dyn Error>> {
-    let mut wtr = csv::Writer::from_path(file_path)?;
-    for record in data {
-        wtr.write_record(record.format())?;
-    }
-    wtr.flush()?;
-    Ok(())
+pub fn headers<'a>() -> Vec<&'a str> {
+    vec![
+        "applicant",
+        "name",
+        "surname1",
+        "surname2",
+        "birthday",
+        "specialty",
+        "vacancy",
+        "quantity",
+        "shift",
+        "department",
+        "required_specialty",
+        "profile",
+        "preference",
+    ]
 }
--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,20 @@ fn main() {
     // initialize logger
     env_logger::init();
 
-    // read applicants requests
+    // define file paths
     let applicants_path: &str = "files/applicants.csv";
+    let vacancies_path: &str = "files/vacancies.csv";
+    let preferences_path: &str = "files/preferences.csv";
+    let board_path: &str = "results/board.csv";
+
+    // read applicants requests file
     let applicants: Vec<Applicant> = parse_file(applicants_path).unwrap_or_else(|err| {
         println!("{err}");
         process::exit(1);
     });
     info!("Loaded {} applicant requests.", applicants.len());
 
-    // read vacant positions
-    let vacancies_path: &str = "files/vacancies.csv";
+    // read vacant positions file
     let vacancies: Vec<Vacancy> = parse_file(vacancies_path).unwrap_or_else(|err| {
         println!("{err}");
         process::exit(1);
@@ -24,19 +28,18 @@ fn main() {
     info!("Loaded {} vacancy positions.", vacancies.len());
 
     // build requests board
-    let preferences_path: &str = "files/preferences.csv";
-    let mut rdr = csv::Reader::from_path(preferences_path).unwrap();
     let mut requests: Vec<Request> = Vec::new();
+    let mut rdr = csv::Reader::from_path(preferences_path).unwrap();
     for row in rdr.records() {
-        let record: Request = Request::new(row.unwrap(), applicants.as_ref(), vacancies.as_ref());
+        let record: Request = Request::build(row.unwrap(), applicants.as_ref(), vacancies.as_ref());
         requests.push(record);
     }
     info!("Loaded {} requests.", requests.len());
 
     // write requests board to file
-    let board_path = "results/board.csv";
-    match export(board_path, requests) {
-        Ok(_) => info!("Requests board successfully written."),
-        Err(err) => error!("{}", err),
+    if let Err(e) = export(board_path, requests) {
+        error!("{}", e);
+        process::exit(1)
     }
+    info!("Requests board successfully written.")
 }
